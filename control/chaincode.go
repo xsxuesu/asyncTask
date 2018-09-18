47d192046091d78ad906d165e27706b99a28f984
depengli <ldpfieldlee@gmail.com>
2018-09-18 20:28:13 +0800

    change tmp = var

--- control/chaincode.go
+++ control/chaincode.go
@@ -4,153 +4,154 @@
 package control
 
 import (
-	"strconv"
-	"strings"
+	"asyncTask/helpers"
+	"asyncTask/model"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"path"
-	"fmt"
 	"os"
-	"io"
-	"asyncTask/helpers"
-	"io/ioutil"
-	"encoding/json"
-	"asyncTask/model"
+	"path"
+	"strconv"
+	"strings"
 )
 
 type ChaincodeUploadByGitParam struct {
 	ChaincodeName string `json:"chaincodeName"`
 	ChaincodePath string `json:"chaincodePath"`
-	ChaincodeGit string `json:"chaincodeGit"`
-} 
-func (this * BaseControl) ChaincodeUploadByZip(w http.ResponseWriter, r *http.Request){
+	ChaincodeGit  string `json:"chaincodeGit"`
+}
+
+func (this *BaseControl) ChaincodeUploadByZip(w http.ResponseWriter, r *http.Request) {
 	if "POST" != r.Method {
 		showUploadHtml(w)
 		return
 	}
 	file, header, err := r.FormFile("chaincodeZip")
-	if err != nil{
-		helpers.ResponseJson(402,err.Error(),helpers.EmptyResult,w)
+	if err != nil {
+		helpers.ResponseJson(402, err.Error(), helpers.EmptyResult, w)
 		return
 	}
 	fileSuffix := path.Ext(header.Filename) //获取文件后缀
 	fmt.Println(fileSuffix)
 	if fileSuffix != ".zip" {
-		panic(model.CustomException{406,"目前只允许zip文件!"})
+		panic(model.CustomException{406, "目前只允许zip文件!"})
 	}
 	fpath := r.FormValue("chaincodePath")
 	if fpath == "" {
-		panic(model.CustomException{402,"path 不能为空"})
+		panic(model.CustomException{402, "path 不能为空"})
 	}
 	name := r.FormValue("chaincodeName")
 	if name == "" {
-		panic(model.CustomException{402,"name 不能为空"})
+		panic(model.CustomException{402, "name 不能为空"})
 	}
 	name = ""
 	defer file.Close()
-	if !helpers.IsDirExists(fpath+"/"+name) {
-		err := os.MkdirAll(fpath+"/"+name,os.ModePerm)
+	if !helpers.IsDirExists(fpath + "/" + name) {
+		err := os.MkdirAll(fpath+"/"+name, os.ModePerm)
 		if err != nil {
-			panic(model.CustomException{403,err.Error()})
+			panic(model.CustomException{403, err.Error()})
 		}
 	}
-	f,err:=os.Create("/tmp/"+header.Filename)
-	if err != nil{
-		panic(model.CustomException{403,err.Error()})
+	f, err := os.Create("/var/" + header.Filename)
+	if err != nil {
+		panic(model.CustomException{403, err.Error()})
 	}
 	defer f.Close()
-	_,err = io.Copy(f,file)
+	_, err = io.Copy(f, file)
 	if err != nil {
-		panic(model.CustomException{403,err.Error()})
+		panic(model.CustomException{403, err.Error()})
 	}
 	//_,err2 := helpers.ExecShell("cd /tmp/ && unzip -o "+header.Filename + " -d "+fpath+"/"+name+" && rm -rf "+header.Filename)
-	tmpPath := "/tmp/"+r.FormValue("chaincodeName")
+	tmpPath := "/var/" + r.FormValue("chaincodeName")
 	fmt.Println(tmpPath)
-	err = os.MkdirAll(tmpPath,os.ModePerm)
-	if err != nil{
-		panic(model.CustomException{405,err.Error()})
-	}
-	_,err = helpers.ExecShell("cd /tmp/ && unzip -o "+header.Filename + " -d "+tmpPath+" && rm -rf "+header.Filename)
+	err = os.MkdirAll(tmpPath, os.ModePerm)
 	if err != nil {
-		panic(model.CustomException{405,"解压文件失败:"+err.Error()})
+		panic(model.CustomException{405, err.Error()})
 	}
-	result,err3 := helpers.ExecShell("ls " + tmpPath)
+	_, err = helpers.ExecShell("cd /var/ && unzip -o " + header.Filename + " -d " + tmpPath + " && rm -rf " + header.Filename)
+	if err != nil {
+		panic(model.CustomException{405, "解压文件失败:" + err.Error()})
+	}
+	result, err3 := helpers.ExecShell("ls " + tmpPath)
 	if err3 != nil {
-		panic(model.CustomException{405,"文件错误:"+err3.Error()})
+		panic(model.CustomException{405, "文件错误:" + err3.Error()})
 		return
 	}
-	if strings.Contains(result,"main.go"){
-		panic(model.CustomException{405,"智能合约格式不正确:"})
+	if strings.Contains(result, "main.go") {
+		panic(model.CustomException{405, "智能合约格式不正确:"})
 	}
 	//智能合约里的文件夹数量
-	cmd := fmt.Sprintf("ls %s -l |grep '^d'|wc -l",tmpPath+"/"+name)
-	s,_ := helpers.ExecShell(cmd)
-	dirCount,_:= strconv.Atoi(strings.Replace(s,"\n","",1))
+	cmd := fmt.Sprintf("ls %s -l |grep '^d'|wc -l", tmpPath+"/"+name)
+	s, _ := helpers.ExecShell(cmd)
+	dirCount, _ := strconv.Atoi(strings.Replace(s, "\n", "", 1))
 	//智能合约里的文件数量
-	cmd = fmt.Sprintf("ls %s -l |grep '^-'|wc -l",tmpPath+"/"+name)
-	s,_ = helpers.ExecShell(cmd)
-	fileCount,_:= strconv.Atoi(strings.Replace(s,"\n","",1))
+	cmd = fmt.Sprintf("ls %s -l |grep '^-'|wc -l", tmpPath+"/"+name)
+	s, _ = helpers.ExecShell(cmd)
+	fileCount, _ := strconv.Atoi(strings.Replace(s, "\n", "", 1))
 	newDirName := ""
-	if fileCount<=0 && dirCount == 1{
-		newDirName = strings.Replace(result,"\n","",1)
-		if !helpers.IsDirExists(fpath+"/"+newDirName){
-			os.MkdirAll(fpath+"/"+newDirName,os.ModePerm)
-		}else{
-			helpers.ExecShell("rm -rf " + fpath+"/"+newDirName+"/*")
+	if fileCount <= 0 && dirCount == 1 {
+		newDirName = strings.Replace(result, "\n", "", 1)
+		if !helpers.IsDirExists(fpath + "/" + newDirName) {
+			os.MkdirAll(fpath+"/"+newDirName, os.ModePerm)
+		} else {
+			helpers.ExecShell("rm -rf " + fpath + "/" + newDirName + "/*")
 		}
 		//将文件夹子目录移到当前目录
-		_,err = helpers.ExecShell("cp -rf " + tmpPath+"/"+newDirName+"/* "+fpath+"/"+newDirName+" && rm -rf "+tmpPath)
+		_, err = helpers.ExecShell("cp -rf " + tmpPath + "/" + newDirName + "/* " + fpath + "/" + newDirName + " && rm -rf " + tmpPath)
 		if err != nil {
-			panic(model.CustomException{405,"解压文件失败:"+err.Error()})
+			panic(model.CustomException{405, "解压文件失败:" + err.Error()})
 		}
-		result,_= helpers.ExecShell("ls " + fpath+"/"+newDirName)
+		result, _ = helpers.ExecShell("ls " + fpath + "/" + newDirName)
 	}
-	if !strings.Contains(result,"main.go"){
-		panic(model.CustomException{405,"智能合约格式不正确"})
+	if !strings.Contains(result, "main.go") {
+		panic(model.CustomException{405, "智能合约格式不正确"})
 	}
 	//同步返回
-	helpers.ResponseJson(200,"上传文件成功!",map[string] interface{}{ "path":fpath+"/"+newDirName},w)
+	helpers.ResponseJson(200, "上传文件成功!", map[string]interface{}{"path": fpath + "/" + newDirName}, w)
 	return
 }
 
-func (this * BaseControl) ChaincodeUploadByGit(w http.ResponseWriter, r *http.Request){
+func (this *BaseControl) ChaincodeUploadByGit(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	log.Println(string(body))
-	if err != nil{
-		panic(model.CustomException{402,"params error"})
+	if err != nil {
+		panic(model.CustomException{402, "params error"})
 	}
 	var u ChaincodeUploadByGitParam
-	err = json.Unmarshal(body,&u)
-	if err != nil{
-		panic(model.CustomException{402,"params error"+err.Error()})
+	err = json.Unmarshal(body, &u)
+	if err != nil {
+		panic(model.CustomException{402, "params error" + err.Error()})
 	}
 	//cpath := u.ChaincodePath+"/"+u.ChaincodeName
 	cpath := u.ChaincodePath
-	tmpPath := "/tmp/"+u.ChaincodeName
-	os.MkdirAll(tmpPath,os.ModePerm)
-	command := "cd "+tmpPath+" && git clone "+u.ChaincodeGit
-	res,err :=helpers.ExecShell(command)
-	if err != nil{
-		helpers.ResponseJson(500,err.Error(),helpers.EmptyResult,w)
+	tmpPath := "/var/" + u.ChaincodeName
+	os.MkdirAll(tmpPath, os.ModePerm)
+	command := "cd " + tmpPath + " && git clone " + u.ChaincodeGit
+	res, err := helpers.ExecShell(command)
+	if err != nil {
+		helpers.ResponseJson(500, err.Error(), helpers.EmptyResult, w)
 		return
 	}
-	result,err3 := helpers.ExecShell("ls " + tmpPath)
+	result, err3 := helpers.ExecShell("ls " + tmpPath)
 	if err3 != nil {
-		panic(model.CustomException{500,"文件错误"+err3.Error()})
+		panic(model.CustomException{500, "文件错误" + err3.Error()})
 	}
-	newDirName := strings.Replace(result,"\n","",1)
-	if !helpers.IsDirExists(cpath+"/"+newDirName){
-		os.MkdirAll(cpath+"/"+newDirName,os.ModePerm)
-	}else{
-		helpers.ExecShell("rm -rf " + cpath+"/"+newDirName+"/*")
+	newDirName := strings.Replace(result, "\n", "", 1)
+	if !helpers.IsDirExists(cpath + "/" + newDirName) {
+		os.MkdirAll(cpath+"/"+newDirName, os.ModePerm)
+	} else {
+		helpers.ExecShell("rm -rf " + cpath + "/" + newDirName + "/*")
 	}
-	_,err = helpers.ExecShell("cp -rf " + tmpPath+"/"+newDirName+"/* "+cpath+"/"+newDirName+" && rm -rf "+tmpPath)
+	_, err = helpers.ExecShell("cp -rf " + tmpPath + "/" + newDirName + "/* " + cpath + "/" + newDirName + " && rm -rf " + tmpPath)
 	if err != nil {
-		panic(model.CustomException{500,"上传失败"+err.Error()})
+		panic(model.CustomException{500, "上传失败" + err.Error()})
 	}
-	result,_= helpers.ExecShell("ls " + cpath+"/"+newDirName)
-	if !strings.Contains(result,"main.go"){
-		panic(model.CustomException{500,"智能合约格式不正确"})
+	result, _ = helpers.ExecShell("ls " + cpath + "/" + newDirName)
+	if !strings.Contains(result, "main.go") {
+		panic(model.CustomException{500, "智能合约格式不正确"})
 	}
-	helpers.ResponseJson(200,"ok",map[string] interface{}{"logs":res,"path":cpath+newDirName},w)
+	helpers.ResponseJson(200, "ok", map[string]interface{}{"logs": res, "path": cpath + newDirName}, w)
 }
--- control/shell.go
+++ control/shell.go
@@ -4,46 +4,46 @@
 package control
 
 import (
-	"net/http"
-	"io/ioutil"
-	"encoding/json"
-	"os"
 	"asyncTask/helpers"
 	"asyncTask/model"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 type ShellExecuteParam struct {
 	ShellContent []byte `json:"shellContent"`
 }
 
-func (this * BaseControl) ShellExecute(w http.ResponseWriter, r *http.Request){
+func (this *BaseControl) ShellExecute(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
-	if err != nil{
+	if err != nil {
 		panic(model.CustomException{500, "params error"})
 	}
 	var u SdkExecuteParam
-	err1 := json.Unmarshal(body,&u)
-	if err1 != nil{
+	err1 := json.Unmarshal(body, &u)
+	if err1 != nil {
 		panic(model.CustomException{402, "params error"})
 	}
-	var command = "rm -rf /tmp/execute.sh"
-	res,err :=helpers.ExecShell(command)
-	if err != nil{
+	var command = "rm -rf /var/execute.sh"
+	res, err := helpers.ExecShell(command)
+	if err != nil {
 		panic(model.CustomException{400, err.Error()})
 	}
-	f,err := os.Create("/tmp/execute.sh")
+	f, err := os.Create("/var/execute.sh")
 	defer f.Close()
 	if err != nil {
 		panic(model.CustomException{410, err.Error()})
 	}
-	_,err = f.Write(u.ShellContent)
+	_, err = f.Write(u.ShellContent)
 	if err != nil {
 		panic(model.CustomException{410, err.Error()})
 	}
-	command = "chmod a+x /tmp/execute.sh && sh /tmp/execute.sh"
-	res,err =helpers.ExecShell(command)
-	if err != nil{
+	command = "chmod a+x /var/execute.sh && sh /var/execute.sh"
+	res, err = helpers.ExecShell(command)
+	if err != nil {
 		panic(model.CustomException{410, err.Error()})
 	}
-	helpers.ResponseJson(200,"ok",map[string] interface{}{"logs":res},w)
+	helpers.ResponseJson(200, "ok", map[string]interface{}{"logs": res}, w)
 }
